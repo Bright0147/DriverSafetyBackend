8ade5546cfbd5e4099ec8020f19aef837aee229e
Bright Msiska <111065839+Bright0147@users.noreply.github.com>
2026-06-30 07:55:03 +0200

    Add password_reset_router to __all__ exports

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,5 +1,6 @@
 from app.routers.auth import router as auth_router
 from app.routers.users import router as users_router
 from app.routers.trips import router as trips_router
+from app.routers.password_reset import router as password_reset_router
 
-__all__ = ["auth_router", "users_router", "trips_router"] 
+__all__ = ["auth_router", "users_router", "trips_router", "password_reset_router"]
